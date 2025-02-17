ac582076927eed10047981d41e04a57a4636c60f
AlexJKL <alexandr_sidorenko@icloud.com>
2025-02-17 19:27:52 +0200

    3.6: Phonebook backend step 6

--- index.js
+++ index.js
@@ -69,16 +69,16 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if(!body.name) {
+    if(!body.name || !body.number) {
         return response.status(400).json({
             error: 'name is missing'
         })
     }
 
-    if(!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        })
+    if(persons.some(person => person.name === body.name)) {
+           return response.status(400).json({
+            error: 'name must be unique'
+        })     
     }
 
     const person = {
