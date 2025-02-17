23c60e8ecc1364af31b13fac199e5f614a787d67
AlexJKL <alexandr_sidorenko@icloud.com>
2025-02-17 14:16:39 +0200

    3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -26,14 +26,6 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
-
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(person => person.id === id)
@@ -51,40 +43,6 @@ app.get('/info', (request, response) => {
         <p>${new Date()}</p>
     `;
     response.send(info)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const person = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-
-    persons = persons.concat(person)
-  
-    response.json(person)
 })
   
 const PORT = 3001
